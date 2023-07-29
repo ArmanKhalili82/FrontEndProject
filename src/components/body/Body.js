99b237fc067b7c50e1499f337cf4c05c9c786ec9
ArmanKhalili82 <hykhalili@yahoo.com>
2023-07-29 15:30:06 +0200

    make it better

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -9,7 +9,7 @@ const Body = () => {
   return (
     <section className='goal'>
         <div className='container-fluid'>
-            <div className='row text-white text-center'>
+            <div className='row text-center'>
                 <div className='col m-4'>
                     <h1 className='display-4 mb-4'>Our Goal</h1>
                     <div className='underline mb-4'></div>
@@ -17,7 +17,7 @@ const Body = () => {
                 </div>
             </div>
 
-            <div className='row my-5'>
+            <div className='row my-5 information'>
               <div className='col-md-4 text-center'>
                 <BsFillCartCheckFill style={{color: 'red', width: '5em', height: '5em'}} />
                 <h1 className='text-white mb-3'>Shop</h1>
